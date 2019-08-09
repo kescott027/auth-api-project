d4ebb90e85ec7a689de9a1be5031101fe1093ee1
kyleesc <kyleesc@amazon.com>
2019-08-09 12:00:35 -0500

    rework of async crypto process

--- src/createuser.ts
+++ src/createuser.ts
@@ -1,6 +1,8 @@
 import { DynamoDB } from 'aws-sdk';
 import * as crypto from 'crypto';
+import { resolve } from 'url';
 
+const ddb = new DynamoDB.DocumentClient()
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -14,131 +16,102 @@ import * as crypto from 'crypto';
  * 
  */
 
-const ddb = new DynamoDB.DocumentClient()
-// const ses = new SES()
 
-export const computeHash = (password: string, salt: crypto.BinaryLike, fn: any): any => {
+export const saveDbRecord = (DBrecord: JSON, DBtable: string, fn: any): any => {
+  let err;
+  try {
+    console.log('Preparing to save data');
+    const params = {
+      TableName: DBtable,
+      Item: DBrecord
+    }
+    
+    const results = ddb.put(params).promise();
+    return results;
+
+  } catch (err) {
+    console.log('error saving to db: ' + err);
+    return err;
+  }
+}
+
+export const computeHash = async (password: string, salt: crypto.BinaryLike, fn: any): Promise<any> => {
   let keylen = 128;
   let iterations = 4096;
   let digest = 'sha256';
   let err;
 
-    if (!password || !salt || !fn) {
-      if (!salt) {
-        err = 'invalid salt or no salt found in hashing algorithm'
-      } else if (!password) {
-        err = 'invalid password or no password found for hashing algorithm'
-      } else {
-        err = 'invalid calilback or no callback found for hashing algorithm'
-      }
-      console.log(err);
+  if (!password) {
+    console.log('no password provied to computeHash function');
+    err = 'invalid password or no password found for hashing algorithm';
+    return err;
+  }
+
+  if (salt == undefined) {
+    const salter = await generateSalt(1024, function (err: string, buf: Buffer) {
       try {
-        fn(err);  
-      } catch {
-        console.log('failure returning error to callback or no callback specified');
+        console.log("no salt provided - generating new salt...");
+        console.log(`salt gen substring: ${buf.toString('base64').substring(0, 15)}`);
+        crypto.pbkdf2(password, buf.toString('base64'), iterations, keylen, digest, fn);
+    } catch (err) {
+        console.log('crpto.pbkdf2Sync computeHash function failed to generate salt.');
+        return err;
       }
-      } else {
-      try {
+    })
+  } else
+    try {
         crypto.pbkdf2(password, salt, iterations, keylen, digest, fn);
-      } catch (err) {
-        console.log('crpto.pbkdf2 hashing failed in function hash - crypto support possibly disabled');
-        return fn(err);
+    } catch (err) {
+        console.log('crpto.pbkdf2Sync hashing failed in function hash - crypto support possibly disabled');
+        return err;
       }
         
-  };
-}
+    };
 
 export const generateSalt = (keylen: number, fn: any): any => {
+  console.log(`...generating ${keylen} random bytes`);
   try {
   crypto.randomBytes(keylen, fn);
   } catch (err) {
     console.log("error generating salt");
   }  
-  }
+}
 
 export const handler = async (event: any = {}): Promise<any> => {
 
   console.log("creating user...");
-  let response, record;
-  let salt: string;
-  let hashpassword: string;
-  let username: string;
-  let rawpassword: string;
+  let response: any;
+  let record: any;
 
-  if (!event.body) {
-    if (!event.pathParameters.username) {
-      console.log("Error: no path parameters")
-      response = {
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        statusCode: 406,
+  let mask = 'xxxxxxxx';
+  let maskedJSON = event.body;
+  maskedJSON.password = mask;
+  console.log("JSON: " + JSON.stringify(maskedJSON));
+
+  let username = JSON.stringify(event.body.username);
+  let rawpassword = JSON.stringify(event.body.password);
+  let salt: any 
+
+  record = await computeHash(rawpassword, salt, function (err: string, derivedKey: Buffer): JSON {
+    console.log("generating hash");
+    let hash = derivedKey.toString('base64');
+    record = {
+      "username": username,
+      "hashrecord": {
+        "hash": hash,
+        "salt": salt
       }
-      return response;
-    } else {
-      username = event.pathParameters.username;
-      console.log("username: " + username);
     }
-  
-  } else {
-    console.log("processing JSON payload");
-    console.log("JSON: " + JSON.stringify(event.body));
-    let data = JSON.parse(event.body);
-    username = data.username;
-    rawpassword = data.password;
-    generateSalt(1024, function (err: string, buf: Buffer) {
-      if (err) throw err;
-      salt = buf.toString('base64');
-      console.log(`${buf.length} bytes of random data: ${buf.toString('hex')}`);
-      computeHash(rawpassword, salt, async function (err: string, hash: any) {
-        if (err) {
-          console.log('Error in hash: ' + err);
-        } else {
-          hashpassword = hash.toString
-          console.log(`generated hash as ${hashpassword} for userId ${username}`)
-
-          record = {
-            "username": username,
-            "hashrecord": {
-              "hash": hashpassword,
-              "salt": salt
-            }
-          };
-
-          console.log("building record: " + record);
-          try {
-            console.log('Preparing to save data');
-            const params = {
-              TableName: 'AuthTable',
-              Item: record
-            }
-            
-            const results = await ddb.put(params).promise();
       
-            if (!results) {
-              console.log('No results found')
-              response = {
-                headers: { 'Access-Control-Allow-Origin': '*' },
-                statusCode: 400,
-              }
-            } else {
-              console.log("results not 400 error")
-              response = {
-                'statusCode': 200,
-                'body': JSON.stringify(record)
-              }
-            }
-          } catch (err) {
-            response = {
-              headers: { 'Access-Control-Allow-Origin': '*' },
-              body: err,
-              statusCode: 500
-            }
-            console.log(err)
-            // return err
-          }
-          return response;  
-
-        }
-      });
-    });
+    console.log(`hash gen substring: ${hash.substring(0, 15)}`);
+    return record;
+  });
+  
+  response = {
+      'statusCode': 200,
+      'body': JSON.stringify(record)
   }
+  console.log(`record is ${record}`);
+  return response;
 }
+
